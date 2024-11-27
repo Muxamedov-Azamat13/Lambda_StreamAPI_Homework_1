83f0e901637584492fe3d1a2ce7812052d93f52e
Muxamedov Azamat <muxamedovazamat13@gmail.com git config --global user.email>
2024-11-27 10:02:14 +0500

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,9 @@ package org.example;
 import java.util.*;
 
 public class Main {
-    private final static Scanner scanner = new Scanner(System.in);
-
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
         double average = list.stream()
                 .filter(avg -> avg % 2 == 0)
