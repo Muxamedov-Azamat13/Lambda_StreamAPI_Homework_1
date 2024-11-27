843dd915be3b1fb3172118976b7fc5ca00fc822b
Muxamedov Azamat <muxamedovazamat13@gmail.com git config --global user.email>
2024-11-27 10:01:26 +0500

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        double average = list.stream()
+                .filter(avg -> avg % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+        System.out.println("Среднее значение чисел = " + average);
+
     }
+
 }
